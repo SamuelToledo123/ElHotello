6ec2a2ae802792897d52abded173633d69def701
Martin Harrysson <MartinHarrysson1990@hotmail.com>
2023-12-13 12:31:14 +0100

    fixed the booking page

--- El Hotello/src/GUI/BookRoomPanel.java
+++ El Hotello/src/GUI/BookRoomPanel.java
@@ -4,28 +4,43 @@ import javax.swing.*;
 import java.awt.*;
 
 public class BookRoomPanel extends JPanel {
+    private String roomInfoText = """
+            lyxigt rum med badkar 
+            bla
+            bla
+            bla
+            bla
+            499 kr/natt
+            """;
+    private JTextArea roomInfo = new JTextArea(roomInfoText);
     private JPanel textAndFields = new JPanel();
     private JLabel name = new JLabel("namn:");
     private JTextField writeName = new JTextField();
-    private JLabel amountOfNights = new JLabel("namn:");
+    private JLabel amountOfNights = new JLabel("How many nights do you wish to stay?");
     private JTextField writeAmountOfNigths = new JTextField();
 
     private JPanel buttonPanel = new JPanel();
-    private JButton book = new JButton("Book now");
+    private JButton book = new JButton("Pay now");
     private JButton cancel = new JButton("Cancel");
 
     public BookRoomPanel(Frame frame){
         setLayout(new BorderLayout());
 
-        textAndFields.setLayout(new GridLayout(2,1));
+        textAndFields.setLayout(new GridLayout(2,2));
+        textAndFields.setSize(new Dimension(500,20));
+        textAndFields.add(name);
+        textAndFields.add(writeName);
+        textAndFields.add(amountOfNights);
+        textAndFields.add(writeAmountOfNigths);
 
         buttonPanel.setSize(new Dimension(500,20));
         buttonPanel.setLayout(new GridLayout(1,2));
         buttonPanel.add(book);
         buttonPanel.add(cancel);
 
-        add(name);
-        add(writeName);
+
+        add(textAndFields, BorderLayout.NORTH);
+        add(roomInfo, BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
 
     }
